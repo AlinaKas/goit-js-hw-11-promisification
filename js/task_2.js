e75ed7a12520431e8ed3a98ff7b20d5d46e37399
Alina Kasianova <avkasyanova@gmail.com>
2021-08-16 15:15:54 +0300

    Добавлен вариант решения
    Из занятия с ментором

--- js/task_2.js
+++ js/task_2.js
@@ -46,3 +46,23 @@ toggleUserState(users, 'Lux').then(logger);
 // toggleUserState(users, 'Lux').then(logger);
 
 
+// ТРЕТИЙ ВАРИАНТ из занятия с ментором
+
+// const users = [
+//   { name: 'Mango', active: true },
+//   { name: 'Poly', active: false },
+//   { name: 'Ajax', active: true },
+//   { name: 'Lux', active: false },
+// ];
+
+// const toggleUserState = async (allUsers, userName) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user,
+//   );
+//   return updatedUsers;
+// };
+
+// const logger = updatedUsers => console.table(updatedUsers);
+
+// toggleUserState(users, 'Mango').then(logger);
+// toggleUserState(users, 'Lux').then(logger);
